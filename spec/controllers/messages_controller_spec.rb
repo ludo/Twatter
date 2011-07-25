d7e856137f66b1093f24d5038eec87efa21758ec
Ludo van den Boom <ludo@cubicphuse.nl>
2011-07-25 21:43:15 +0200

    Explicit message testing. Why failing?

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,23 +1,29 @@
 require 'spec_helper'
 
 describe MessagesController do
-  it "should have a 'new' action" do
-    get :new
-    response.should be_success
-  end
+  describe "GET /messages/new" do
+    before(:each) do
+      @message = Message.new
+    end
 
-  it "should render the 'new' template" do
-    get :new
-    response.should render_template("new")
-  end
+    it "should have a 'new' action" do
+      get :new
+      response.should be_success
+    end
 
-  it "should initialize a new message" do
-    Message.should_receive(:new)
-    get :new
-  end
+    it "should render the 'new' template" do
+      get :new
+      response.should render_template("new")
+    end
 
-  it "should expose a new message to the view" do
-    get :new
-    assigns[:message].should_not be_nil
+    it "should initialize a new message" do
+      Message.should_receive(:new)
+      get :new
+    end
+
+    it "should expose a new message to the view" do
+      get :new
+      assigns[:message].should == @message
+    end
   end
 end
