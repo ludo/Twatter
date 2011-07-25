23b19cd851e42e298120de56bafcb7660b7d87e8
Ludo van den Boom <ludo@cubicphuse.nl>
2011-07-25 21:53:14 +0200

    Failing controller spec for post /messages

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -29,7 +29,11 @@ describe MessagesController do
   end
 
   describe "POST /messages" do
-    it "should initialize a new message from params"
+    it "should initialize a new message from params" do
+      Message.should_receive(:new).with(:body => "Something interesting")
+      post :create, :body => "Something interesting"
+    end
+
     it "should save the message"
     it "should redirect to shared messages list"
   end
